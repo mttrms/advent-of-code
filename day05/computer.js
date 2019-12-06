f913417c1f548fb5d911fe9f39296bcca6501507
Matt Ramos <mattramos@gmail.com>
2019-12-05 21:51:33 -0500

    Complete part 1

--- day05/computer.js
+++ day05/computer.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const instructions = fs.readFileSync('input.txt', 'utf8')
                    .split(',').map(val => parseInt(val));
 let input = 1;
+let output; 
 
 const processOpCode = (opCode) => {
   let newOpCode = String(opCode);
@@ -16,5 +17,47 @@ const processOpCode = (opCode) => {
 for (let i = 0; i < instructions.length; i++) {
   const instruction = processOpCode(instructions[i]);
   const opCode = instruction.slice(instruction.length - 2);
+
+  const paramOne = instruction[2];
+  const paramTwo = instruction[1];
+  const paramThree = instruction[0];
+
+  switch (opCode) {
+    case '01':
+      valueOne = paramOne == true ? instructions[i + 1] : instructions[(instructions[i + 1])];
+      valueTwo = paramTwo == true ? instructions[i + 2] : instructions[(instructions[i + 2])];
+
+      instructions[instructions[i + 3]] = valueOne + valueTwo;
+
+      i += 3;
+      break;
+    case '02':
+      valueOne = paramOne == true ? instructions[i + 1] : instructions[(instructions[i + 1])];
+      valueTwo = paramTwo == true ? instructions[i + 2] : instructions[(instructions[i + 2])];
+
+      instructions[instructions[i + 3]] = valueOne * valueTwo;
+
+      i += 3;
+      break;
+    case '03':
+      instructions[instructions[i + 1]] = input;
+
+      i += 1;
+      break;
+    case '04':
+      if (paramOne == true) {
+        let value = instructions[i + 1];
+        output = value;
+      } else {
+        let position = instructions[i + 1];
+        output = instructions[position];
+      }
+
+      i += 1;
+      break;
+    case '99':
+      console.log(output);
+      return;
+  }
 }
 
