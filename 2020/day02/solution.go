fa74f2798860b1fb07cb2c8a4d451782268588c5
Matt Ramos <mattramos@gmail.com>
2020-12-03 00:33:29 -0500

    gofmt day2

--- 2020/day02/solution.go
+++ 2020/day02/solution.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
-  "strings"
 	"strconv"
+	"strings"
 )
 
 type Password struct {
-  character string
-  maximum int
-  minimum int
-  password string
+	character string
+	maximum   int
+	minimum   int
+	password  string
 }
 
 func main() {
@@ -24,83 +24,83 @@ func main() {
 	}
 	defer file.Close()
 
-  formattedPasswords := formatPasswords(file)
-  invalidPasswords := countInvalidPasswords(formattedPasswords)
-  validPasswords := len(formattedPasswords) - invalidPasswords
+	formattedPasswords := formatPasswords(file)
+	invalidPasswords := countInvalidPasswords(formattedPasswords)
+	validPasswords := len(formattedPasswords) - invalidPasswords
 
-  newInvalidPasswords := countNewInvalidPasswords(formattedPasswords)
-  newValidPasswords := len(formattedPasswords) - newInvalidPasswords
+	newInvalidPasswords := countNewInvalidPasswords(formattedPasswords)
+	newValidPasswords := len(formattedPasswords) - newInvalidPasswords
 
-  fmt.Printf("Part 1 - There are %v valid passwords\n", validPasswords)
-  fmt.Printf("Part 2 - There are %v valid passwords\n", newValidPasswords)
+	fmt.Printf("Part 1 - There are %v valid passwords\n", validPasswords)
+	fmt.Printf("Part 2 - There are %v valid passwords\n", newValidPasswords)
 }
 
 func formatPasswords(file *os.File) []Password {
-  var pw []Password
+	var pw []Password
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-    unparsedPw := scanner.Text()
-    splitPw := strings.Split(unparsedPw, " ")
+		unparsedPw := scanner.Text()
+		splitPw := strings.Split(unparsedPw, " ")
 
-    pwRange := strings.Split(splitPw[0], "-")
-    pwChar := strings.Trim(splitPw[1], ":")
-    password := splitPw[2]
-    min, _ := strconv.Atoi(pwRange[0])
-    max, _ := strconv.Atoi(pwRange[1])
+		pwRange := strings.Split(splitPw[0], "-")
+		pwChar := strings.Trim(splitPw[1], ":")
+		password := splitPw[2]
+		min, _ := strconv.Atoi(pwRange[0])
+		max, _ := strconv.Atoi(pwRange[1])
 
-    var p Password
-    p.character = pwChar
-    p.maximum = max
-    p.minimum = min
-    p.password = password
+		var p Password
+		p.character = pwChar
+		p.maximum = max
+		p.minimum = min
+		p.password = password
 
-    pw = append(pw, p)
+		pw = append(pw, p)
 	}
 
-  return pw
+	return pw
 }
 
 func isPasswordInvalid(password Password) bool {
-  charUsage := strings.Count(password.password, password.character)
+	charUsage := strings.Count(password.password, password.character)
 
-  return charUsage < password.minimum || charUsage > password.maximum
+	return charUsage < password.minimum || charUsage > password.maximum
 }
 
 func isNewPasswordInvalid(p Password) bool {
-  validCount := 0
-  i := p.minimum - 1
-  j := p.maximum - 1
+	validCount := 0
+	i := p.minimum - 1
+	j := p.maximum - 1
 
-  if string(p.password[i]) == p.character {
-    validCount++
-  }
+	if string(p.password[i]) == p.character {
+		validCount++
+	}
 
-  if string(p.password[j]) == p.character {
-    validCount++
-  }
+	if string(p.password[j]) == p.character {
+		validCount++
+	}
 
-  return validCount != 1
+	return validCount != 1
 }
 
 func countInvalidPasswords(passwords []Password) int {
-  i := 0
-  for _, pw := range passwords {
-    if (isPasswordInvalid(pw)) {
-      i++
-    }
-  }
+	i := 0
+	for _, pw := range passwords {
+		if isPasswordInvalid(pw) {
+			i++
+		}
+	}
 
-    return i
+	return i
 }
 
 func countNewInvalidPasswords(passwords []Password) int {
-  i := 0
-  for _, pw := range passwords {
-    if (isNewPasswordInvalid(pw)) {
-      i++
-    }
-  }
+	i := 0
+	for _, pw := range passwords {
+		if isNewPasswordInvalid(pw) {
+			i++
+		}
+	}
 
-  return i
+	return i
 }
